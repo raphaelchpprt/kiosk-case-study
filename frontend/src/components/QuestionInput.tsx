c802156b0c8603e7dc0b79abdcbb75d95f3a7fc8
Raphael Chappert <raphael.chappert@hotmail.fr>
2025-12-11 16:29:34 +0100

    Implement input rendering for Text and Number question types in QuestionInput component WIP

--- frontend/src/components/QuestionInput.tsx
+++ frontend/src/components/QuestionInput.tsx
@@ -1,20 +1,22 @@
 'use client';
 import { Question } from '@/src/types/question';
+import { TextInput, NumberInput, Select, Textarea } from '@mantine/core';
 
 interface QuestionInputProps {
   question: Question;
   value: string | number;
 }
 export function QuestionInput({ question, value }: QuestionInputProps) {
+  //je prends le label en anglais pour le mvp
+  const label = question.labelEn;
+
   switch (question.content) {
     case 'Text':
-      return null;
+      return <TextInput label={label} value={value} />;
     case 'number':
-      return null;
+      return <NumberInput label={label} value={value} />;
     case 'enum':
-      return null;
-    case 'Table':
-      return null;
+      return <TextInput label={label} value={value} />;
     default:
       return null;
   }
