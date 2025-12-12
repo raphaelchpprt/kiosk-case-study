f38af90a8a1bfd13dd362ceeec035c34df9e0fa3
Raphael Chappert <raphael.chappert@hotmail.fr>
2025-12-12 09:12:47 +0100

    Refactor QuestionInput component to finalize and include onChange prop and improve input handling

--- frontend/src/components/QuestionInput.tsx
+++ frontend/src/components/QuestionInput.tsx
@@ -5,18 +5,56 @@ import { TextInput, NumberInput, Select } from '@mantine/core';
 interface QuestionInputProps {
   question: Question;
   value: string | number;
+  onChange: (value: string | number) => void;
 }
-export function QuestionInput({ question, value }: QuestionInputProps) {
-  //je prends le label en anglais pour le mvp
-  const label = question.labelEn;
-
+export function QuestionInput({
+  question,
+  value,
+  onChange,
+}: QuestionInputProps) {
+  console.log('🚀 ~ value:', value);
   switch (question.content) {
     case 'Text':
-      return <TextInput label={label} value={value} />;
+      return (
+        <TextInput
+          key={question.id}
+          value={typeof value === 'string' ? value : ''}
+          size="lg"
+          placeholder="Enter text"
+          onChange={(e) => onChange(e.currentTarget.value)}
+        />
+      );
     case 'number':
-      return <NumberInput label={label} value={value} />;
+      return (
+        <NumberInput
+          key={question.id}
+          value={typeof value === 'number' ? value : undefined}
+          onChange={(val) => onChange(val || 0)}
+          size="lg"
+          rightSection={question.unit}
+          placeholder="Enter a number"
+          min={0}
+        />
+      );
     case 'enum':
-      return <Select label={label} value={value} />;
+      const options =
+        question.enumOptionsEn?.map((opt) => ({
+          value: opt,
+          label: opt,
+        })) || [];
+
+      return (
+        <Select
+          key={question.id}
+          value={typeof value === 'string' ? value : null}
+          onChange={(val) => onChange(val || '')}
+          data={options}
+          placeholder="Select an option"
+          searchable
+          clearable
+          size="lg"
+        />
+      );
     default:
       return null;
   }
